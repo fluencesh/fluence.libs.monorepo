42b5407af349988ec2a5acc29f892bfd48225797
Andrew Zubko <andrew@zubko.biz>
2017-11-21 10:56:45 +0200

    fix plugin

--- packages/multivest.blockchain.ethereum/src/plugin.blockchain.ethereum.js
+++ packages/multivest.blockchain.ethereum/src/plugin.blockchain.ethereum.js
@@ -1,7 +1,14 @@
 const { AbstractPlugin } = require('@applicature/multivest.core');
 
 class EthereumBlockchainPlugin extends AbstractPlugin {
+    constructor(pluginManager) {
+        super(pluginManager, 'blockchain.ethereum');
+    }
 
+// eslint-disable-next-line class-methods-use-this
+    init() {
+
+    }
 }
 
 module.exports = EthereumBlockchainPlugin;

eee9ee8b1d0d5a4255d94d9be5f9ce400af68a64
Andrew Zubko <andrew@zubko.biz>
2017-11-26 22:31:23 +0200

    fix exporting jobs

--- packages/multivest.blockchain.bitcoin/index.js
+++ packages/multivest.blockchain.bitcoin/index.js
@@ -5,8 +5,8 @@ const BitcoinService = require('./src/services/blockchain/bitcoin');
 const BitcoinTxMiningListener = require('./src/jobs/btc.tx.mining.listener');
 const BitcoinTransactionSender = require('./src/jobs/btc.tx.sender');
 
-const CompatibleBitcoinTxMiningListener = require('./src/jobs/btc.tx.mining.listener');
-const CompatibleBitcoinTransactionSender = require('./src/jobs/btc.tx.sender');
+const CompatibleBitcoinTxMiningListener = require('./src/jobs/compatible.btc.tx.mining.listener');
+const CompatibleBitcoinTransactionSender = require('./src/jobs/compatible.btc.tx.sender');
 
 
 const BitcoinConstant = require('./src/services/constants/bitcoin.constant');

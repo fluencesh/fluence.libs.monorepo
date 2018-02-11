503fc374a4e65d09e9a7ef1b0a2bb7dd6ad25e9d
Andrew Zubko <dev@zubko.biz>
2018-02-11 19:33:02 +0200

    update

--- packages/multivest.blockchain.ethereum/test/integration/integration.test.ts
+++ packages/multivest.blockchain.ethereum/test/integration/integration.test.ts
@@ -3,7 +3,8 @@ import { Contract } from '../../src/services/contracts/contract';
 
 describe('service', () => {
     it('should sign', async () => {
-        const service = new EthereumBlockchainService(false);
+
+        const service = new EthereumBlockchainService(null, false, false);
         const signed = await service.sign(
             '0xab096838b9c8af3a35e384d426bba055f93e1f83',
             'fjls'

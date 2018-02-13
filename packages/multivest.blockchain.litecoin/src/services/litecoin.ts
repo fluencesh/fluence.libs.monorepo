1d3b293704321db5d8a812ace954486d5379fff9
Andrew Zubko <dev@zubko.biz>
2018-02-13 02:57:52 +0200

    register litecoin testnet too

--- packages/multivest.blockchain.litecoin/src/services/litecoin.ts
+++ packages/multivest.blockchain.litecoin/src/services/litecoin.ts
@@ -2,6 +2,11 @@ import * as Client from 'bitcoin-core';
 import * as bitcoin from 'bitcoinjs-lib';
 import * as config from 'config';
 
+import * as BitcoinjsTestnets from 'bitcoinjs-testnets';
+
+BitcoinjsTestnets.register(bitcoin.networks);
+
+
 import { BlockchainService } from '@applicature-restricted/multivest.blockchain';
 import { BitcoinBlockchainService } from '@applicature-restricted/multivest.blockchain.bitcoin';
 
@@ -17,7 +22,7 @@ export class LitecoinBlockchainService extends BitcoinBlockchainService {
             this.client = new Client(config.get('multivest.blockchain.litecoin.providers.native'));
         }
 
-        this.network = bitcoin.networks.litecoin;
+        this.network = bitcoin.networks[config.get('multivest.blockchain.litecoin.network')];
         this.masterPublicKey = config.get('multivest.blockchain.litecoin.hd.masterPublicKey');
     }
 

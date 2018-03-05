7300d9fb889023de3c3a40b099ff1f2109448d79
tkachenko <olexiy.tkachenko@gmail.com>
2018-03-05 16:51:30 +0200

    update block parsing and tests

--- packages/multivest.blockchain.bitcoin/test/integration/integration.test.ts
+++ packages/multivest.blockchain.bitcoin/test/integration/integration.test.ts
@@ -1,5 +1,20 @@
+import { BitcoinBlockchainService } from '../../src/services/blockchain/bitcoin';
+
 describe('test', () => {
-    it('test', () => {
-        expect(1).toBe(1);
+    let service: BitcoinBlockchainService;
+
+    beforeEach(() => {
+        service = new BitcoinBlockchainService();
+    });
+
+    it('should get block by height', async () => {
+        const block = await service.getBlockByHeight(1287103);
+
+        expect(typeof block.difficulty).toEqual('number');
+        expect(typeof block.hash).toEqual('string');
+        expect(block.height).toEqual(1287103);
+        expect(block.network).toEqual('testnet');
+        expect(typeof block.parentHash).toEqual('string');
+        expect(typeof block.time).toEqual('number');
     });
 });

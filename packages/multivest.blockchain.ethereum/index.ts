cc83811f1b4d58e939584f59061026e50da58fd3
Artem Kanaki <kanaki.artem@gmail.com>
2018-04-30 17:33:04 +0300

    import fix

--- packages/multivest.blockchain.ethereum/index.ts
+++ packages/multivest.blockchain.ethereum/index.ts
@@ -12,6 +12,8 @@ export * from './src/services/contracts/contract';
 export * from './src/services/contracts/erc20.contract';
 
 export * from './src/services/objects/contract.service';
+export * from './src/services/objects/ethereum.contract.subscription.service';
+export * from './src/services/objects/ethereum.event.log.service';
 
 export * from './src/services/transports/ethereum.json-rpc.proxy';
 export * from './src/services/transports/ethereum.transport';

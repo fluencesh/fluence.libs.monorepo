0f7fb1f6e2351fa3a883b212a302f5414baf3b1f
Artem Kanaki <kanaki.artem@gmail.com>
2018-04-30 16:06:03 +0300

    test fix

--- packages/multivest.blockchain.ethereum/index.ts
+++ packages/multivest.blockchain.ethereum/index.ts
@@ -1,5 +1,9 @@
 export * from './src/dao/mongodb/contract.dao';
 export * from './src/dao/contract.dao';
+export * from './src/dao/mongodb/ethereum.contract.subscription.dao';
+export * from './src/dao/ethereum.contract.subscription.dao';
+export * from './src/dao/mongodb/ethereum.event.log.dao';
+export * from './src/dao/ethereum.event.log.dao';
 
 export * from './src/services/blockchain/ethereum';
 export * from './src/services/blockchain/managed.ethereum.transport.service';

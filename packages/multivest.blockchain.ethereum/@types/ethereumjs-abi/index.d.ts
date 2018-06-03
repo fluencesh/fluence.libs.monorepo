42c4b5e334d1880911e2e0788a71bcdf5d950c08
Artem Kanaki <artem.kanaki@applicature.com>
2018-06-03 11:44:11 +0000

    Merged in BLC-38_subscription-event-data (pull request #21)
    BLC-38 subscription event data

--- packages/multivest.blockchain.ethereum/@types/ethereumjs-abi/index.d.ts
+++ packages/multivest.blockchain.ethereum/@types/ethereumjs-abi/index.d.ts
@@ -1,4 +1,8 @@
 declare module 'ethereumjs-abi'{ 
-  export function soliditySHA3(types: any, values: any): Buffer; 
-  export function simpleEncode(...args: Array<string>): Buffer; 
+  export function soliditySHA3(types: any, values: any): Buffer;
+  export function simpleEncode(...args: Array<string>): Buffer;
+  export function encode(abi: Array<any>, signature: string, data: Array<string>): Buffer;
+  export function decode(abi: Array<any>, signature: string, data: string): Array<string | Buffer>;
+  export function rawEncode(types: Array<string>, data: Array<string>): Buffer;
+  export function rawDecode(types: Array<string>, data: string | Buffer): Array<string>;
 }

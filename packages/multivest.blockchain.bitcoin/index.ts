7240dad3a88807d38b0d2a2d312020a088b0482a
Artem Kanaki <kanaki.artem@gmail.com>
2018-06-07 17:39:00 +0300

    `ManagedBitcoinTransportService` fix

--- packages/multivest.blockchain.bitcoin/index.ts
+++ packages/multivest.blockchain.bitcoin/index.ts
@@ -2,6 +2,8 @@ export * from './src/jobs/btc.tx.sender';
 export * from './src/jobs/compatible.btc.tx.mining.listener';
 export * from './src/jobs/compatible.btc.tx.sender';
 
+export * from './src/constants';
+
 export * from './src/services/blockchain/bitcoin';
 
 export * from './src/jobs/btc.tx.sender';

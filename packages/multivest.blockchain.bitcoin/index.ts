b606d49e183d44f3530edb8d0fcc4e5a6dc5961b
Artem Kanaki <kanaki.artem@gmail.com>
2018-06-07 15:12:59 +0300

    export fix

--- packages/multivest.blockchain.bitcoin/index.ts
+++ packages/multivest.blockchain.bitcoin/index.ts
@@ -4,6 +4,10 @@ export * from './src/jobs/compatible.btc.tx.sender';
 
 export * from './src/services/blockchain/bitcoin';
 
+export * from './src/jobs/btc.tx.sender';
+export * from './src/jobs/compatible.btc.tx.mining.listener';
+export * from './src/jobs/compatible.btc.tx.sender';
+
 export * from './src/services/transports/abstract.bitcoin.transport';
 export * from './src/services/transports/bc.bitcoin.transport';
 export * from './src/services/transports/bi.bitcoin.transport';

41489061195f7bebf07f7e7c1ec6a90dadef747e
Artem Kanaki <kanaki.artem@gmail.com>
2018-07-31 19:52:50 +0300

    init

---
+++ packages/fluence.ms/test/spec/unit/metric.controller.spec.ts
@@ -0,0 +1,8 @@
+import { MetricController } from '../../../src';
+import { ResponseMock } from '../../mocks/response.mock';
+
+describe('case', () => {
+    it('should work', () => {
+        expect(1).toEqual(1);
+    });
+});

be7cce4b91053ec14ba92ca5e2599a2ffbe64686
Artem Kanaki <kanaki.artem@gmail.com>
2018-08-03 14:17:26 +0300

    improvements

--- packages/fluence.ms/src/middlewares/aws.auth.middleware.ts
+++ packages/fluence.ms/src/middlewares/aws.auth.middleware.ts
@@ -1,11 +1,18 @@
 import { NextFunction, Response } from 'express';
+import { get } from 'lodash';
+import { MultivestError } from '../../node_modules/@applicature-private/multivest.core';
+import { Errors } from '../errors';
 import { AwsRequest } from '../types';
 import { AuthMiddleware } from './auth.middleware';
 
 export class AwsAuthMiddleware extends AuthMiddleware {
     public async attachProjectAndClient(req: AwsRequest, res: Response, next: NextFunction) {
-        req.relatedClient = req.apiGateway.context.client;
-        req.project = req.apiGateway.context.project;
+        req.relatedClient = get(req, 'apiGateway.context.client', null);
+        req.project = get(req, 'apiGateway.context.project', null);
+
+        if (!req.relatedClient) {
+            return next(new MultivestError(Errors.FORBIDDEN, 403));
+        }
 
         next();
     }

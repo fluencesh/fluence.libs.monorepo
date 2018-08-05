60d3bf1b0aafb76b36300595b1fd6bee96af09e8
Artem Kanaki <kanaki.artem@gmail.com>
2018-08-05 11:49:05 +0300

    sw controler fix

--- packages/fluence.ms/src/controllers/swagger.controller.ts
+++ packages/fluence.ms/src/controllers/swagger.controller.ts
@@ -1,5 +1,6 @@
 import { NextFunction, Response } from 'express';
 import { createReadStream, readFile } from 'fs';
+import { generateSwaggerYaml } from 'swapi';
 import { ProjectRequest, SwResponseFormat } from '../types';
 
 export class SwaggerController {
@@ -10,24 +11,6 @@ export class SwaggerController {
     }
 
     public async getSwaggerFile(req: ProjectRequest, res: Response, next: NextFunction) {
-        const asBuffer = req.query.buffer === SwResponseFormat.Buffer;
-
-        if (asBuffer) {
-            const swReadStream = createReadStream(this.swPath, { encoding: 'utf8' });
-
-            swReadStream.pipe(res);
-        } else {
-            const api = await new Promise((resolve, reject) => {
-                readFile(this.swPath, { encoding: 'utf8' }, (err, chunk) => {
-                    if (err) {
-                        reject(err);
-                    } else {
-                        resolve(chunk);
-                    }
-                });
-            });
-
-            res.status(200).send(api);
-        }
+        res.status(200).send(generateSwaggerYaml());
     }
 }

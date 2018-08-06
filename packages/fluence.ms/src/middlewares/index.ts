6ff1418e6b15f4ca6386caade6e5d1d01e60ead5
Artem Kanaki <kanaki.artem@gmail.com>
2018-08-06 15:42:34 +0300

    fix

--- packages/fluence.ms/src/middlewares/index.ts
+++ packages/fluence.ms/src/middlewares/index.ts
@@ -1,10 +1,7 @@
 export * from './admin.access.middleware';
 export * from './error.handler.middleware';
-export * from './metric.middleware';
-export * from './metric.service.auth.middleware';
 export * from './timeout.middleware';
 
 export * from './auth.middleware';
 export * from './aws.auth.middleware';
 export * from './header.auth.middleware';
-export * from './request.auth.middleware';

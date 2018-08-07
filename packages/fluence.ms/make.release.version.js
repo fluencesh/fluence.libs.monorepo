c003cabcf8239a764b231b6faeb65e6c715073b4
Artem Kanaki <kanaki.artem@gmail.com>
2018-08-07 13:46:48 +0300

    pipeline fix

--- packages/fluence.ms/make.release.version.js
+++
@@ -1,5 +0,0 @@
-const fs = require('fs');
-
-const packageJson = require('./package.json');
-packageJson.version = packageJson.version.replace(/-beta\..*$/, '');
-fs.writeFileSync('./package.json', JSON.stringify(packageJson, null, 2), { encoding: 'utf8' });

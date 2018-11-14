45e2992555254d3a993b29ca91ecaca687d6b826
Artem Kanaki <kanaki.artem@gmail.com>
2018-11-14 15:58:36 +0200

    [^] for version upd

--- packages/fluence.lib.ethereum/src/index.ts
+++ packages/fluence.lib.ethereum/src/index.ts
@@ -1,5 +1,5 @@
 export * from './constants';
 export * from './errors';
 export * from './plugin.blockchain.ethereum';
-export * from './services';
 export * from './types';
+export * from './services';

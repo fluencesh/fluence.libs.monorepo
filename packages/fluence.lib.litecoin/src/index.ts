2169107097fbeb45b7848fbbee5b4caeb865d409
Artem Kanaki <artem.kanaki@applicature.com>
2018-11-16 17:07:17 +0200

    [*] v upd

--- packages/fluence.lib.litecoin/src/index.ts
+++ packages/fluence.lib.litecoin/src/index.ts
@@ -1,5 +1,5 @@
 export * from './constants';
 export * from './errors';
 export * from './plugin.blockchain.litecoin';
-export * from './services';
 export * from './types';
+export * from './services';

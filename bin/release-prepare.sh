4103216ffca6196bed78fd5b0cad95a6b72b30bc
Andrew Zubko <andrew@applicature.com>
2018-11-30 06:46:32 -0800

    pass github url with token to lerna

--- bin/release-prepare.sh
+++ bin/release-prepare.sh
@@ -10,7 +10,7 @@ setup_git() {
     git config --global user.email "travis@travis-ci.org"
     git config --global user.name "Travis CI"
 
-    git checkout -b $TRAVIS_BRANCH
+    git checkout -b $TRAVIS_JOB_ID
 }
 
 setup_npm

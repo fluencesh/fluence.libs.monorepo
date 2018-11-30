161d963a2ef7ed500e69f98e7a81bee4814914a7
Andrew Zubko <andrew@applicature.com>
2018-11-30 06:35:26 -0800

    checkout to new branch

--- bin/release-prepare.sh
+++ bin/release-prepare.sh
@@ -9,6 +9,8 @@ setup_npm() {
 setup_git() {
     git config --global user.email "travis@travis-ci.org"
     git config --global user.name "Travis CI"
+
+    git checkout -b $TRAVIS_BRANCH
 }
 
 setup_npm
